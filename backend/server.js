7d0c8c36688a09253ad22a6eb8ca7e1f062dad66
battinans <battinans@gmail.com>
2025-06-11 07:32:32 +0000

    prisma + db changes (for git reference)

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
 const authRoutes = require("./routes/authRoutes");
 const outpassRoutes = require("./routes/outpassRoutes");
 
